94d3a9efd628a72369d7116a632b931bdee3d74f
s22714 <s22714@pjwstk.edu.pl>
2023-12-31 20:39:27 +0100

    init

---
+++ Random/MainMenu.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class MainMenu
+    {
+        public void OpenMainMenu()
+        {
+            ConsoleKeyInfo key;
+
+            int selection = 0;
+            List<string> list = new List<string>() 
+            {
+                "<<Start>>",
+                "<<Exit>>"
+            };
+            while (true)
+            {
+                Console.Clear();
+                DrawLogo();
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if(i == selection)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    Console.WriteLine(list[i]);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                key = Console.ReadKey();
+
+                if(key.Key == ConsoleKey.W ||  key.Key == ConsoleKey.UpArrow) 
+                {
+                    selection--;
+                    if(selection < 0) selection = list.Count - 1;
+                }
+                if (key.Key == ConsoleKey.S || key.Key == ConsoleKey.DownArrow)
+                {
+                    selection++;
+                    if (selection >= list.Count) selection = 0;
+                }
+                if(key.Key == ConsoleKey.Enter || key.Key == ConsoleKey.Spacebar)
+                {
+                    if(selection == 0)
+                    {
+                        break;
+                    }
+                    if (selection == 1)
+                    {
+                        Environment.Exit(0);
+                    }
+                }
+                
+            }
+            
+        }
+
+        public void DrawLogo()
+        {
+            Console.WriteLine(@"
+================================================================================================
+███████╗██╗      ██████╗ ██████╗ ██████╗ ██╗   ██╗    ██████╗ ██╗     ██╗   ██╗██████╗ ██████╗ 
+██╔════╝██║     ██╔═══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝    ██╔══██╗██║     ██║   ██║██╔══██╗██╔══██╗
+█████╗  ██║     ██║   ██║██████╔╝██████╔╝ ╚████╔╝     ██████╔╝██║     ██║   ██║██████╔╝██║  ██║
+██╔══╝  ██║     ██║   ██║██╔══██╗██╔═══╝   ╚██╔╝      ██╔══██╗██║     ██║   ██║██╔══██╗██║  ██║
+██║     ███████╗╚██████╔╝██║  ██║██║        ██║       ██████╔╝███████╗╚██████╔╝██║  ██║██████╔╝
+╚═╝     ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝        ╚═╝       ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ 
+================================================================================================
+");
+        }
+        public void GameOver(int score)
+        {
+            Console.Clear();
+            Console.WriteLine($@"
+===========================================================================
+ ██████╗  █████╗ ███╗   ███╗███████╗     ██████╗ ██╗   ██╗███████╗██████╗ 
+██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██╔═══██╗██║   ██║██╔════╝██╔══██╗
+██║  ███╗███████║██╔████╔██║█████╗      ██║   ██║██║   ██║█████╗  ██████╔╝
+██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║   ██║╚██╗ ██╔╝██╔══╝  ██╔══██╗
+╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ╚██████╔╝ ╚████╔╝ ███████╗██║  ██║
+ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝     ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═╝
+===========================================================================
+                           YOUR SCORE: {score}
+                       Press any key to exit
+");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+    
+}
---
+++ Random/Program.cs
@@ -0,0 +1,149 @@
+﻿using Game;
+
+Console.Title = "FLORPY BLURD";
+
+var Menu = new MainMenu();
+Menu.OpenMainMenu();
+
+int origRow;
+int origCol;
+
+int score = 0;
+
+void WriteAt(string s, int x, int y)
+{
+    Console.SetCursorPosition(origCol + x, origRow + y);
+    Console.Write(s);
+}
+
+Console.Clear();
+origRow = Console.CursorTop;
+origCol = Console.CursorLeft;
+
+int width = 120;
+int height = 28;
+
+Console.SetWindowSize(1, 1);
+Console.SetBufferSize(width, height);
+Console.SetWindowSize(width, height);
+
+var rnd = new Random();
+
+string[,] sciany = new string[width,2];
+
+sciany[width - 1,0] = "#";
+sciany[width - 1, 1] = "4";
+
+int interval = 40;
+int drawWall = 0;
+
+int pos = 10;
+
+Thread t = new Thread(new ThreadStart(PlayerPos));
+t.Start();
+
+Thread t2 = new Thread(new ThreadStart(PlayerFall));
+t2.Start();
+
+int last = 10;
+
+while (true)
+{
+
+    DrawScreen();
+
+    drawWall++;
+    
+    for(int i = 0; i < width; i++)
+    {
+        if (sciany[i,0] == "#")
+        {
+            if(i == 0)
+            {
+                sciany[i, 0] = "";
+                sciany[i, 1] = "";
+            }
+            else
+            {
+                sciany[i - 1, 0] = sciany[i, 0];
+                sciany[i - 1, 1] = sciany[i, 1];
+
+                sciany[i, 0] = "";
+                sciany[i, 1] = "";
+            }
+        }
+    }
+
+    if(interval == drawWall)
+    {
+        drawWall = 0;
+        sciany[width - 1, 0] = "#";
+        sciany[width - 1, 1] = rnd.Next(12,height-8).ToString();
+    }
+    
+}
+
+void DrawScreen()
+{
+
+    if(pos >= height || pos < 0)
+    {
+        Menu.GameOver(score);
+    }
+
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            if (x == pos && y == 10)
+            {
+                WriteAt("@", 10, pos);
+            }
+            if (y == height - 1 || y == 0)
+            {
+                WriteAt("_", x, y);
+            }
+        }
+    }
+    Console.WriteLine("SCORE: " + score.ToString());
+    for (int x = 0; x < width; x++)
+    {
+        if (sciany[x, 0] == "#")
+        {
+
+            for (int y = height - 1; y > 0; y--)
+            {
+                if(!(y <= height - Int32.Parse(sciany[x, 1]) - 1 && y >= height - Int32.Parse(sciany[x, 1]) - 1 - 4 ))
+                    WriteAt("#", x, y);
+            }
+            if(x == 10)
+            {
+                if (!(pos <= height - Int32.Parse(sciany[x, 1]) - 1 && pos >= height - Int32.Parse(sciany[x, 1]) - 1 - 6)) Menu.GameOver(score);
+                else score++;
+            }
+        }
+    }
+    
+    Thread.Sleep(10);
+    Console.Clear();
+    origRow = Console.CursorTop;
+    origCol = Console.CursorLeft;
+}
+
+void PlayerPos()
+{
+    while (true)
+    {
+        if (Console.ReadKey().Key == ConsoleKey.Spacebar) pos-=4;
+    }
+    
+}
+
+void PlayerFall()
+{
+    while (true)
+    {
+        pos++;
+        Thread.Sleep(100);
+    }
+}
